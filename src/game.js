110860ee3e1d7da6441145c7a9cc5c0be95acd9f
amandawidegren <amanda.widegren@greaterthan.eu>
2017-09-26 13:08:35 +0200

    with card.js back again!

--- src/game.js
+++ src/game.js
@@ -2,12 +2,40 @@ import React from 'react';
 import Card from './card'
 import Counter from './counter'
 
-const Game = () => (
-    <div class="row" id="container">
-    <div className="game" id="cardgame">
-    <img id="h1" src="/images/memorygamelogo.png"/>
-    <Counter/>
-    <Card src="/images/image5.jpg" />
+class Game extends React.Component {
+    
+    state = {
+        cards: [
+        { src: "/images/image5.jpg" },
+        { src: "/images/image4.gif" },
+        { src: "/images/image2.jpg" },
+        { src: "/images/image1.gif" }
+        
+        ]
+    }
+    
+
+
+    
+    render() {
+        return (
+
+            <div className="game" id="cardgame">
+            
+                <img id="h1" src="/images/memorygamelogo.png"/>
+                <Card src={this.state.cards[0].src} />
+                <Card src={this.state.cards[1].src} />
+                <Card src={this.state.cards[2].src} />
+                <Card src={this.state.cards[3].src} />
+            </div>
+    
+            
+        )
+    }
+}
+
+    
+    /*<Card src="/images/image5.jpg" />
      <Card src="/images/image4.gif" />
      <Card src="/images/image2.jpg" />
     <Card src="/images/image1.gif" />
@@ -18,9 +46,7 @@ const Game = () => (
     <Card src="/images/image1.gif" />
     <Card src="/images/image6.png" />
      <Card src="/images/image5.jpg" />
-     <Card src="/images/image6.png" />
-</div>
-    </div>
-)
+     <Card src="/images/image6.png" />*/
+
 
 export default Game
